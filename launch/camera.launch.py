2717b2f55cbe81ec31f1a0d3bca208678683fdff
adrianCo <adrian.cobo.mh@gmail.com>
2024-09-18 17:15:00 +0200

    test done
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -8,14 +8,13 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
+    camera_config_yaml=get_package_share_directory('perception_system')+'/config/camera_config.yaml'
+    camera_config_json=get_package_share_directory('perception_system')+'/config/camera_config.json'
+
     camera_with_imu_enabled_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')]),
-        launch_arguments={'use_sim_time': 'false',
-                          'enable_accel': 'true',
-                          'enable_gyro': 'true',
-                          'pointcloud.enable': 'true',
-                          'enable_color': 'true'}.items()
+        launch_arguments={'config_file': camera_config_yaml, 'json_file_path': camera_config_json}.items()
     )
 
     return LaunchDescription([
--- launch/simulated_system.launch.py
+++ launch/simulated_system.launch.py
@@ -11,30 +11,12 @@ def generate_launch_description():
     spawn_system = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('perception_system'), 'launch', 'spawn_systems.launch.py')]))
-    
-    camera_config_yaml=get_package_share_directory('perception_system')+'/config/camera_config.yaml'
-    camera_config_json=get_package_share_directory('perception_system')+'/config/camera_config.json'
-    
-
-    lidar_node = Node(namespace='rslidar_sdk', package='rslidar_sdk', executable='rslidar_sdk_node', output='screen')
-
-    camera_with_imu_enabled_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('realsense2_camera'), 'launch', 'rs_launch.py')]),
-        launch_arguments={'enable_accel': 'True', 'enable_gyro': 'True', 'unite_imu_method': '1'}.items()
-    )
 	
     # constant dt must be equal as scan_period param at lidarslam_ros2 
     imu_filter_node = Node(package='imu_filter_madgwick', executable='imu_filter_madgwick_node', output='screen',
                       parameters=[{'use_mag': False, 'publish_tf': False, 'constant_dt': 0.2 }], remappings=[('/imu/data_raw', '/camera/camera/imu')])
 
-    rviz_config=get_package_share_directory('perception_system')+'/rviz/perception_rviz_config.rviz'
-    rviz_node = Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
-
     return LaunchDescription([
         spawn_system,
-        lidar_node,
-        camera_with_imu_enabled_node,
         imu_filter_node,
-        rviz_node
     ])
